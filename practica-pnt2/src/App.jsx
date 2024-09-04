04dbbdd2e5576eef60733a03fd41ac71c1a27a64
JachiDev <jachithedev@gmail.com>
2024-09-03 22:03:00 -0300

    diapositivas 1 y 2

--- practica-pnt2/src/App.jsx
+++ practica-pnt2/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { Ej_diap1 } from './componentes/ej_diap1'
+import { Ej_diap2 } from './componentes/ej_diap2'
 
 function App() {
  
   return (
     <>
+    <Ej_diap2/>
       <div className='container'>
         <div>
           <h1>Grupo 1</h1>
---
+++ practica-pnt2/src/componentes/ej_diap2.jsx
@@ -0,0 +1,8 @@
+
+export const Ej_diap2 = () => {
+  const auto = ['Mercedes', 'B3', 2024];
+  
+    return (
+    console.log(`Tienes un ${auto[0]} modelo ${auto[1]} año ${auto[2]}`) 
+  )
+}
